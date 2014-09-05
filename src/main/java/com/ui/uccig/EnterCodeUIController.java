c6cb967d6d52801628c2dec1b4d89f8fe1dc0ca5
ravjotsingh <ravjot28@gmail.com>
2014-09-05 02:55:45 -0400

    Added doc opening

--- src/main/java/com/ui/uccig/EnterCodeUIController.java
+++ src/main/java/com/ui/uccig/EnterCodeUIController.java
@@ -281,6 +281,8 @@ public class EnterCodeUIController implements Initializable, IScreenController {
 
     @FXML
     public void submitProposalBinder() {
+        //yaha
+        new Rav("a.docx").execute();
     }
 
     @FXML
