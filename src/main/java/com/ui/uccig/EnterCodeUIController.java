70c64d82fa4226bba5916e7f2d295781b0a25bbf
ravjotsingh <ravjot28@gmail.com>
2014-09-05 12:27:00 -0400

    Added doc opening

--- src/main/java/com/ui/uccig/EnterCodeUIController.java
+++ src/main/java/com/ui/uccig/EnterCodeUIController.java
@@ -35,6 +35,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.util.Duration;
+import ravrun.Rav;
 
 /**
  * FXML Controller class
